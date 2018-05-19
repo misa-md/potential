508cf283ea74d79df04cd2d28d5a7c1682b028fa
genshen <genshenchu@gmail.com>
2018-05-20 00:33:37 +0800

    feat(denpendency): use newest kiwi framework (bundle put/get without MPI_Pack & MPI_Unpack).

---
+++ interpolation_object.h
@@ -0,0 +1,26 @@
+//
+// Created by baihe back to 2015-12-26.
+//
+#ifndef CRYSTAL_MD_INTERPOLATION_OBJECT_H
+#define CRYSTAL_MD_INTERPOLATION_OBJECT_H
+
+class InterpolationObject {
+public:
+    InterpolationObject();
+
+    ~InterpolationObject();
+
+    void initInterpolationObject(int _n, double _x0, double dx, double *data);
+
+    void bcastInterpolationObject(int rank);
+
+    void interpolatefile();
+
+    int n;          //!< 表中数据个数
+    double x0;      //!< 起始点
+    double invDx;   //!< 倒数
+    double *values;
+    double (*spline)[7];
+};
+
+#endif //CRYSTAL_MD_INTERPOLATION_OBJECT_H
