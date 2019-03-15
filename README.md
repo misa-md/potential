b710ffa485ffaf6eaa24abc91543742eac2010f2
genshen <genshenchu@gmail.com>
2019-03-15 11:34:02 +0800

    build(cmake): better cmake configuration, and correct some spell mistakes.

--- README.md
+++ README.md
@@ -13,8 +13,7 @@ $ cd lib-pot
 $ cmake -B./build -H./ \
  -DOpenMP_ENABLE_FLAG=ON \
  -DMPI_ENABLE_FLAG=ON \   # enable mpi
- -DTEST_BUILD_ENABLE_FLAG=ON \
- -DTEST_MPI_ENABLE_FLAG=ON \    # enable unit tests
+ -DTEST_BUILD_ENABLE_FLAG=ON \ # enable unit tests
  -DTEST_MPI_ENABLE_FLAG=OFF
 $ cmake --build ./build -j 4
 ```
--- config.cmake
+++ config.cmake
@@ -14,13 +14,15 @@ option(OpenMP_ENABLE_FLAG "Use OpenMP" OFF) #change this flag to OFF to disable 
 option(MPI_ENABLE_FLAG "Use MPI library" ON) #change this flag to false to disable mpi
 option(TEST_BUILD_ENABLE_FLAG "Enable building test" ON) # enable test
 option(TEST_MPI_ENABLE_FLAG "Enable MPI in test" ON) # enable mpi in test, its value depends on option MPI_ENABLE_FLAG.
-option(DEBUG_ENABLE_FLAG "Enable tools building" ON) # enable debug mode building.
 
 ## architecture ralated values.
 option(POT_ARCH_SW "Enable sunway athread" OFF) # enable sunway athread if its running on sunway system.
 
-if (DEBUG_ENABLE_FLAG)
-    set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -Wall")
+set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -Wall")
+set(CMAKE_C_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -Wall")
+
+if (CMAKE_BUILD_TYPE MATCHES "^(Debug|DEBUG|debug)$")
+    set(POT_DEBUG_ENABLE_FLAG ON)
 endif ()
 
 #############
